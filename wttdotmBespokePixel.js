eedc52041f85bea06acae88d3c2c89fe3e560d7b
Morry Kolman <wttdotm@gmail.com>
2025-06-18 16:28:22 -0400

    add bespoke pixel

--- wttdotmBespokePixel.js
+++ wttdotmBespokePixel.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {  
-    console.log("DOMContentLoaded at page ", window.location.pathname.substring(1));
+document.addEventListener("load", function() {  
+    console.log("loaded at page ", window.location.pathname.substring(1));
     fetch(`https://wemakeinter.net/diyPixel/morryProjPixel.php?event=pageView&project=mainSite&page=${window.location.pathname.substring(1)}`)
 });
